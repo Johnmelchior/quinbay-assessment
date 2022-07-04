eb2ef5f1bbed2799247458e9e34da5a271904aa8
Sacred Heart <john@Sacreds-MacBook-Pro.local>
2022-07-04 22:06:30 +0530

    Code clean

--- api/server.js
+++ api/server.js
@@ -1,4 +1,3 @@
-const proxy = require("express-http-proxy");
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const session = require('express-session');
 
@@ -15,28 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../build')));
 app.enable("trust proxy");
 
-/*app.use(
-  "/service/blibliserver",
-  proxy("https://www.blibli.com", {
-    proxyReqOptDecorator(proxyReqOpts) {
-      proxyReqOpts.headers["Origin"] = "https://www.blibli.com";
-      return proxyReqOpts;
-    },
-    proxyErrorHandler: function(err, res, next) {
-      console.log("Proxy err", err);
-      next(err);
-    }
-  })
-)*/
-
-/*app.get('/backend/search/products', (req, res) => {
-  const json = req.query;
-  request(`https://www.blibli.com/backend/search/products?searchTerm=${json.searchTerm}&start=${json.start}&itemPerPage=${json.itemPerPage}`,
-    (err, res1, data) => {
-      console.log(err, res1, data);
-      res.json(res1);
-    }, (error) => console.error(error));
-});*/
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Credentials", true);
